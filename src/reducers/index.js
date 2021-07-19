9f74ba1729cd20d9031c3c713efbe8b7ae14693f
Sahil Saini <46193454+sahil137@users.noreply.github.com>
2021-07-19 22:58:17 +0530

    change state to object

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,16 @@
 import { ADD_MOVIES } from "../actions";
-export default function movies (state = [], action){
+
+const initalMovieState = {
+    list: [],
+    favourites: []
+}
+
+export default function movies (state = initalMovieState, action){
     if (action.type === ADD_MOVIES){
-        return action.movies;
+        return {
+            ...state,
+            list: action.movies
+        }
     }
     return state;
 }
