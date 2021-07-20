cd55e045f42ecd2d26b8168ba5292ea20e231beb
Sahil Saini <46193454+sahil137@users.noreply.github.com>
2021-07-20 16:05:54 +0530

    add combineReducers() method

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,11 @@
-import { ADD_MOVIES, ADD_TO_FAVOURITES, REMOVE_FROM_FAVOURITES, SET_SHOW_FAVOURITES } from "../actions";
+import { combineReducers } from "redux";
+
+import { 
+    ADD_MOVIES, 
+    ADD_TO_FAVOURITES, 
+    REMOVE_FROM_FAVOURITES, 
+    SET_SHOW_FAVOURITES } 
+    from "../actions";
 
 const initalMovieState = {
     list: [],
@@ -52,14 +59,19 @@ export function search(state = initialSearchState, action){
     return state;
 }
 
-const initialRootState = {
-    movies: initalMovieState,
-    search: initialSearchState
-}
+// const initialRootState = {
+//     movies: initalMovieState,
+//     search: initialSearchState
+// }
 
-export default function rootReducer(state = initialRootState, action){
-    return{
-        movies: movies(state.movies, action),
-        search: search(state.search, action)
-    }
-}
+// export default function rootReducer(state = initialRootState, action){
+//     return{
+//         movies: movies(state.movies, action),
+//         search: search(state.search, action)
+//     }
+// }
+
+export default combineReducers({
+    movies,
+    search
+})
